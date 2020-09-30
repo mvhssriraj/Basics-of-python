be1d34a00156b0a8200da31487aabf535229f364
mvhssriraj <72150046+mvhssriraj@users.noreply.github.com>
2020-09-30 21:04:48 +0530

    basic calc and text
    from the above file you will be able to learn how to do calculations in python and how to text in  python

---
+++ Basics calc text.py
@@ -0,0 +1,65 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #welcome to python world
+>>> #dear beginners
+>>> #now you will be learning the basics of python
+>>> #first you will learn how to print
+>>> print("Welcome! to python world")
+Welcome! to python world
+>>> #this is the basic code you have learnt
+>>> #python is nothing but a calculator
+>>> #lets now see how it works
+>>> x = 100
+>>> y= 250
+>>> #now you want the sum of x and y
+>>> z= x+y
+>>> print(z)
+350
+>>> #congratulations you have learn the basics of python
+>>> #now i am introducing it with different functions
+>>> a= x/y
+>>> print(a)
+0.4
+>>> b=x*y
+>>> print(b)
+25000
+>>> c=x-y
+>>> print(c)
+-150
+>>> #now i will introduce you to ** which means power off
+>>> d=x**y
+>>> #d is the x power y
+>>> print(d)
+100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+>>> #now introducing to // which means quoitent in python
+>>> #on dividing 3 with 2 you will get 1 as quoitent
+>>> #come on lets see
+>>> #come on lets see with our previous inputs
+>>> e=x//y
+>>> print(e)
+0
+>>> #now lets learn % is the remainder in python
+>>> f=x%y
+>>> print(f)
+100
+>>> #congratulations you have almost completed 20% basics of pythoon
+>>> #congratulations you have almost completed 20% basics of pythoon
+>>> #now we will learn how to print statements
+>>> print("congratulations")
+congratulations
+>>> print('congratulations')
+congratulations
+>>> #did you understand the differeence between the two inputs
+>>> # yes you can use " as well as'
+>>> #now lets see if you can use both at a time or not
+>>> print("congratulations')
+      
+SyntaxError: EOL while scanning string literal
+>>> #oh no as you can see syntax erroe
+>>> #we cant use both the statements
+>>> #i.e we can use " and ' ata a time
+>>> #now i will show you how you will put a appostropus
+>>> print("he\'s great")
+he's great
+>>> #this is how you creat he's not simply writing he's but to write he\'s
+>>> #from the above knowledge you are now familiar with basic calc. and texting
