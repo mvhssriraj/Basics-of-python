e12bf101ea9f3117584369d7364f66c220a4662e
mlscmoulika <53378071+mlscmoulika@users.noreply.github.com>
2020-09-30 23:00:15 +0530

    added a code for if-else too
    sum and if-else too

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -3,3 +3,14 @@ y=input("\nenter number 2:\n")
 z=int(x)+int(y)
 print("\nthe sum of the two numbers is:\n")
 print(z)
+# If the number is positive, we print an appropriate message
+
+n = 4
+if n > 0:
+    print(n, "is a positive number.")
+print("This is always printed.")
+
+n = -21
+if n > 0:
+    print(n, "is a positive number.")
+print("This is also always printed.")
